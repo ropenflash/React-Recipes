06803e9e6078b7a90f5d5177e9a7d3c1da7b1c0a
EC2 Default User <ec2-user@ip-172-31-20-54.us-east-2.compute.internal>
2019-05-27 10:47:03 +0000

    mounted root compenent

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const HtmlWebpackPlugin=require("html-webpack-plugin")
+const path=require("path")
+
+
 module.exports={
-    entry:"./index.js",
+    entry:"./src/index.js",
     output:{
-        path:__dirname,
+        path:path.resolve(__dirname,'dist'),
         filename:'./bundle.js'
         
     },
@@ -9,10 +13,23 @@ module.exports={
         rules:[
             {
                 
-                test:'/\.js$/',
-                loader:'babel-loader'
+                test:/\.js$/,
+                exclude:/node_modules/,
+                loader:'babel-loader',
             }
             
             ]
-    }
+    },
+    
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html',
+            inject:'body'
+        })
+        ],
+    
+    devServer: {
+  compress: true,
+  public: '27adebdb87d747a089e96faabb0d57e0.vfs.cloud9.us-east-2.amazonaws.com' // That solved it
+}
 }
