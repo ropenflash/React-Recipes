48466ca8f7e7f4ed7291f9ffd7ff4a54cbff752c
EC2 Default User <ec2-user@ip-172-31-20-54.us-east-2.compute.internal>
2019-05-29 13:06:27 +0000

    nav bar  added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const HeaderLink = ({ children, ...props }) => (
+  <NavLink
+    className="p1 mx2 black rounded text-decoration-none "
+    activeClassName="bg-white"
+    {...props}
+  >
+    {' '}
+    {children}
+  </NavLink>
+);
 
 const Header = () => (
-  <header className="px4">
+  <header className="flex items-center justify-between px4">
     <h1 className="h1">
       <span role="img">🍔</span>
       MyRecipes
     </h1>
+    <nav>
+      <HeaderLink exact to="/">
+        Home
+      </HeaderLink>
+      <HeaderLink exact to="/Favorites">
+        Favorites
+      </HeaderLink>
+    </nav>
   </header>
 );
+
+HeaderLink.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Header;
